aa6f3ab05dcefb55ff453012510634f8293ade1c
Norasjo <nora.sjolie@icloud.com>
2026-09-25 13:32:58 +0200

    fix in antiwindup function

--- part_1/controller.py
+++ part_1/controller.py
@@ -231,7 +231,7 @@ class DPController:
         is applied to.
         """
         tau_applied = np.asarray(tau_applied, dtype=float).reshape(6)
-        diff_xy_ned = tau_applied[:2] - self._tau_unsat[:2] - self._tau_unsat[:2]   # still BODY here...
+        diff_xy_ned = tau_applied[:2] -  self._tau_unsat[:2]  
         # tau is in BODY; integrator states int_ned are in NED, so rotate
         # the correction from BODY back to NED before applying it.
         diff_ned = body_to_ned_xy(diff_xy_ned, psi)
